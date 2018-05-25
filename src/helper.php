0c7e75921e333e1776bbe3c63194d47da6cbcf7c
Ivan Kryukov <vanilich@mail.ru>
2018-05-25 16:34:29 +0300

    append helper class

---
+++ src/helper.php
@@ -0,0 +1,13 @@
+<?php
+	class Helper {
+        public static function check404($url) {
+            $handle = curl_init($url);
+            curl_setopt($handle,  CURLOPT_RETURNTRANSFER, TRUE);
+            $response = curl_exec($handle);
+            $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+            curl_close($handle);
+
+            return $httpCode;
+        }		
+	}
+?>
