aaa37f5afb30a4ec0c821e2ab77d5ea8fbbd93db
Ivan Kryukov <vanilich@mail.ru>
2018-05-25 15:14:45 +0300

    append export to excel

--- src/controller/TableController.php
+++ src/controller/TableController.php
@@ -34,46 +34,73 @@
 		* Выгрузка шаблона в Excel
 		**/
 		public function excel(Request $request, Response $response, array $args) {
+			// Получаем данные от клиента
+			$body = $request->getParsedBody();
+
+			// Марка автомобиля
+			$mark =  isset($body['mark']) ? intval($body['mark']) : 0;
+			// Модель автомобиля
+			$model = isset($body['model']) ? intval($body['model']) : 0;
+			// Магазины
+			$shop =  isset($body['shop']) ? $body['shop'] : [];
+			// Города
+			$city =  isset($body['city']) ? $body['city'] : [];
+
+			// Создаем модель
+		    $priceModel = new PriceModel($this->container->db);	
+		    // Генерируем таблицу
+		    $data = $priceModel->getTable($mark, $model, $shop, $city);
+
+			$shop = $data['shop'];
+			$mark = $data['mark'];
+			$data = $data['data'];
+
 			ob_end_clean();
 
-			/**
-				$city = isset($_COOKIE["city"]) && $_COOKIE["city"] ? $_COOKIE["city"] : 1;
-
-				$dataArray1 = [['Марка','Модель',],];
-				$dataArray2 = [];
-
-				foreach ($data->getShops($city) as $row) {
-				    $dataArray2[] = $row['name'];
-				}
-
-				$dataArray = [];
-				$dataArray[0] = array_merge($dataArray1[0], $dataArray2);
-
-				$dataArray3 = [];
-				$dataArray4 = [];
-				$i = 0;
-				foreach ($data->getModels() as $row) {
-				    $i++;
-				    $dataArray4 = [[$row['car'],$row['model']],] ;
-				    $dataArray5 = [];
-
-				    foreach ($data->getShops($city) as $row_shop) {
-				        $priceInfo = $data->getPriceInfo($row_shop['id'],$row['model_id']);
-				        $dataArray5[] = $priceInfo['price'];
-				    }
-
-				    $dataArray[$i] = array_merge($dataArray4[0], $dataArray5);
-
-				    unset($dataArray4);
-				    unset($dataArray5);
-				}
-
-			**/
-			
+			// Создаем Excel файл
 			$doc = new PHPExcel();
+
 			$doc->setActiveSheetIndex(0);
+			$sheet = $doc->getActiveSheet();
+
+			// Заполняем таблицу марками и моделями автомобилей
+			$sheet->setCellValue("A1", 'Марка');
+			$sheet->setCellValue("B1", 'Модель');
+
+			$i=2;
+            foreach ($data as $key => $value) {
+                $sheet->setCellValue("A$i", $value['mark_name']);
+                $sheet->setCellValue("B$i", $value['model_name']);
+
+                $i++;
+            }
+
+            // Выводим список автосалонов
+            $tmp = [];
+            foreach ($shop as $value) {
+            	$tmp[] = $value['name'];
+            }
+            $sheet->fromArray($tmp, null, 'C1');
+
+            // Цены на автомобили
+            $i=2;
+            foreach ($data as $key => $value) {
+            	$tmp = [];
+            	foreach($value['shop'] as $item) {
+            		if( isset($item['price']) AND !empty($item['price']['price']) AND $item['price']['price'] != 0 ) {
+            			$tmp[] = number_format($item['price']['price'], 0, '', ' ');
+            		} else {
+            			$tmp[] = '';
+            		}
+            		$sheet->fromArray($tmp, null, "C$i");
+            	}
+            	$i++;
+            }
+
+            // Имя Excel файла
 			$filename = 'price-' . date("Y-m-d H:i:s") . '.xls';
 
+			// Отдаем excel файл клиенту
 			header('Content-Type: application/vnd.ms-excel');
 			header('Content-Disposition: attachment;filename="' . $filename . '"');
 			header('Cache-Control: max-age=0');
