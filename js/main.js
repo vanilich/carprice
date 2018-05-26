2f80aefc0e56538841f872aea3c422935ef6ab4c
Ivan Kryukov <vanilich@mail.ru>
2018-05-26 14:50:33 +0300

    append flash message javascript feature

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 (function() {
 
 })();
+
+// Flash messaging feature
+(function() {
+	// Вывод мгновенного сообщения
+	window.showMessage = function(msg, type, duration = 3000) {
+		var message = $('<div/>', {
+		    class: 'flash-message flash-message--' + type,
+		    text: msg
+		});
+
+		$(message).appendTo('body');
+
+		setTimeout(function() {
+			$(message).fadeOut(400);
+		}, duration);
+	}
+
+	// usage
+	window.showMessage('message', 'default');
+})();
